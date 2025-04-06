7c165736758493d19735bef1739cf2dd28aeb3c1
fursov231 <fursov231@hotmail.com>
2025-04-06 18:11:04 +0300

    убрать profile сервисного аккаунта

--- infra/provider.tf
+++ infra/provider.tf
@@ -15,8 +15,6 @@ terraform {
     key    = "tf-state.tfstate"
     encrypt = true
 
-    profile = "rk109_profile"
-
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
