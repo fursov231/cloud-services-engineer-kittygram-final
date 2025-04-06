e4f5d8e38a5d1e6e4175c93805c7f5a2902b129f
fursov231 <fursov231@hotmail.com>
2025-04-06 23:15:38 +0300

    поправить s3

--- infra/provider.tf
+++ infra/provider.tf
@@ -14,6 +14,7 @@ terraform {
     region = "ru-central1"
     key    = "tf-state.tfstate"
 
+    encrypt = true
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
